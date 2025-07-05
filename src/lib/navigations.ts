d81794f13e917b7006d2285b10ddb93197e3df1b
Chris Seaman <christopherseaman1983@gmail.com>
2025-07-05 15:42:28 +0100

    update added navigations.ts to lib folder with headerNavdata

---
+++ src/lib/navigations.ts
@@ -0,0 +1,24 @@
+import { Nav } from "../../types"
+
+export const headerNav : Nav[] = [
+  {
+    href: "/",
+    title: "Home",
+  },
+  {
+    href: "/drainage-services",
+    title: "Drainage Services",
+  },
+  {
+    href: "/plumbing-services",
+    title: "Plumbing Services",
+  },
+  {
+    href: "/contact",
+    title: "Contact",
+  },
+  {
+    href: "/terms-and-conditions",
+    title: "Terms and Conditions",
+  },
+]
